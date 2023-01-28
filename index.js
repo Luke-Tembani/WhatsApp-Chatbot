36287e404562ea52dd8f8c677245d2cc410d5694
LukeFj <luke@figjammobile.com>
2023-01-28 13:19:57 +0200

    axios text body

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post("/webhook",(req,res)=>{
                         messaging_product:"whatsapp",
                         to:from,
                         text:{
-                            body:msg_body
+                            body:"Hello from Code Dev Team"
                         }
                     
                     },
