feaa9aa35ba515a04f2b2b46f0aee8f74ec6e3fd
LukeFj <luke@figjammobile.com>
2023-01-30 14:10:34 +0200

    added db connection to JAWS Added Products Retrived Products Added Switch Case

---
+++ db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+
+const connection = mysql.createConnection({
+    host:process.env.JAWSDB_HOST,
+    user:process.env.JAWSDB_USER,
+    password:process.env.JAWSDB_PASSWORD,
+    database:process.env.JAWSDB_DB_NAME
+});
+
+
+connection.connect((err)=>{
+    if(err){
+        console.log("Error"+err);
+    }else{
+        console.log("Connected Successfully")
+    }
+});
+
+
+module.exports  = connection;
--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express=require("express");
 const body_parser=require("body-parser");
 const axios=require("axios");
 require('dotenv').config();
+const db = require("./db");
 
 const app=express().use(body_parser.json());
 
 const token=process.env.TOKEN;
 const mytoken=process.env.MYTOKEN;//prasath_token
 
-app.listen(process.env.PORT,()=>{
+app.listen(8000 || process.env.PORT,()=>{
     console.log("webhook is listening");
 });
 
@@ -60,11 +61,27 @@ app.post("/webhook",(req,res)=>{ //i want some
                console.log("boady param "+msg_body);
 
 
-               if(msg_body === "Hie" || "Hello" || "Hey" || "Howdy" || "Hi" || "hie" || "hi" || "hello" || "howdy"){
+            //    if(msg_body === "Hie" || "Hello" || "Hey" || "Howdy" || "Hi" || "hie" || "hi" || "hello" || "howdy"){
 
-                greeting(phon_no_id, from);
-                sendMenu(phon_no_id, from);
-               }
+            //     greeting(phon_no_id, from);
+            //     sendMenu(phon_no_id, from);
+            //    }
+
+
+            switch(msg_body){
+                case "meds": getMedication();
+                             break;
+
+                case "Hi": greeting(phon_no_id, from);
+                        break;
+                        
+                        
+                case "Menu": sendMenu(phon_no_id, from);
+                             break;
+                             
+                             
+                 default: notFound();            
+            }
 
                res.sendStatus(200);
             }
@@ -78,8 +95,59 @@ app.post("/webhook",(req,res)=>{ //i want some
 });
 
 app.get("/",(req,res)=>{
+    getMedication();
     res.status(200).send("Hi This is Code Dev WebHook Setup");
 });
+
+
+function getMedication(){
+    db.query("SELECT * FROM medication",(err,results)=>{
+        if(err){
+            console.log(err);
+        }else{
+
+            for(let i =0; i<= results.length-1;i++){
+                console.log();
+            axios({
+                method: "POST",
+                url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/messages?access_token=" + token,
+                data: {
+                    messaging_product: "whatsapp",
+                    to: from,
+                    text: {
+                        body: results[i].name + " "+ "- $"+results[i].price
+                    }
+                },
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+        }
+           
+        }
+    })
+}
+
+
+function notFound(){
+
+    axios({
+        method: "POST",
+        url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/messages?access_token=" + token,
+        data: {
+            messaging_product: "whatsapp",
+            to: from,
+            text: {
+                body: "Invalid Option"
+            }
+        },
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+}
 
 function greeting(phon_no_id, from) {
     axios({
